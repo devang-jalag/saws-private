7f293abf4b677512accc71083c4f502c8c476430
Tarun Chauhan <tarunchauhan21203@gmail.com>
2026-07-08 00:33:26 -0300

    added common components (navbar, sidebar, dashboard, loader)

---
+++ saws-frontend/src/components/common/Loader.tsx
@@ -0,0 +1,14 @@
+import { CircularProgress, Box } from "@mui/material";
+
+export default function Loader() {
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="60vh"
+        >
+            <CircularProgress />
+        </Box>
+    );
+}

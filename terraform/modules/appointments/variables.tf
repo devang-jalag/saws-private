d0fb845da1a7eda1add8cab1858b42ddd4847846
Devang Jalag <zj799583@dal.ca>
2026-07-08 12:37:15 -0300

    infra: define terraform variables for appointments module

---
+++ terraform/modules/appointments/variables.tf
@@ -0,0 +1,11 @@
+variable "env" {
+  description = "Environment name (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}

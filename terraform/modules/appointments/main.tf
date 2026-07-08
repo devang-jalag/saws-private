f664a6e4152b64cca2a748c569d20736d911d5b0
Devang Jalag <zj799583@dal.ca>
2026-07-08 14:32:09 -0300

    update: temp credentials and IAM labrole

--- terraform/modules/appointments/main.tf
+++ terraform/modules/appointments/main.tf
@@ -44,61 +44,10 @@ resource "aws_dynamodb_table" "appointments" {
   }
 }
 
-# IAM Role for Lambdas
+# IAM Role for Lambdas (Learner Lab requires using the pre-existing LabRole)
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "appointments_lambda_exec_role_${var.env}"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_policy" "lambda_dynamodb" {
-  name = "appointments_dynamodb_policy_${var.env}"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = [
-        "dynamodb:PutItem",
-        "dynamodb:GetItem",
-        "dynamodb:UpdateItem",
-        "dynamodb:Scan",
-        "dynamodb:Query"
-      ]
-      Effect   = "Allow"
-      Resource = [
-        aws_dynamodb_table.services.arn,
-        aws_dynamodb_table.doctors.arn,
-        aws_dynamodb_table.appointments.arn,
-        "${aws_dynamodb_table.appointments.arn}/index/usr_id_index"
-      ]
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy.lambda_dynamodb.arn
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# CloudWatch Logs policy for debugging
-resource "aws_iam_role_policy_attachment" "lambda_cloudwatch" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
 }
 
 # Lambda Functions
@@ -113,7 +62,7 @@ data "archive_file" "lambda_zip" {
 resource "aws_lambda_function" "services_api" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "saws-services-api-${var.env}"
-  role             = aws_iam_role.lambda_exec.arn
+  role             = data.aws_iam_role.lab_role.arn
   handler          = "api.services.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.10"
@@ -128,7 +77,7 @@ resource "aws_lambda_function" "services_api" {
 resource "aws_lambda_function" "doctors_api" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "saws-doctors-api-${var.env}"
-  role             = aws_iam_role.lambda_exec.arn
+  role             = data.aws_iam_role.lab_role.arn
   handler          = "api.doctors.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.10"
@@ -143,7 +92,7 @@ resource "aws_lambda_function" "doctors_api" {
 resource "aws_lambda_function" "appointments_api" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "saws-appointments-api-${var.env}"
-  role             = aws_iam_role.lambda_exec.arn
+  role             = data.aws_iam_role.lab_role.arn
   handler          = "api.appointments.handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.10"
